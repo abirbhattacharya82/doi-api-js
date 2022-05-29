b09c4eb99359bd48422aedc7ad0ffacb4152309d
abirbhattacharya82 <abirbhattacharya82@gmail.com>
2022-05-29 18:51:46 +0530

    CORS

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express=require('express');
+const cors=require('cors');
 const app=express();
 const port=process.env.PORT || 8000
 const imp=require('./data.json')
+app.use(cors);
 
 app.get('/',(req,res)=>{
     res.send("Hello World");
